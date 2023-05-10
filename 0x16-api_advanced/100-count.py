e925aef570277db7ef8e3b728a48879a6f8aaaba
KagontleBooysen <k.booysen@alustudent.com>
2023-05-09 19:53:57 -0700

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
-"""Module for task 3"""
-s
+"""A file to make a query to an endpoint"""
+from requests import request
 
-def count_words(subreddit, word_list, after=None, word_count=None):
-    if word_count is None:
-        word_count = {}
-    if after is None:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    else:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return
-    data = response.json()
-    for post in data['data']['children']:
-        title = post['data']['title'].lower()
+
+def count_words(subreddit, word_list, after="", counter={}, ini=0):
+    """A recursive function that queries the Reddit API, parses the title
+    of all hot articles, and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces. Javascript should count as
+    javascript, but java should not)
+    """
+    if ini == 0:
         for word in word_list:
-            count = title.count(word)
-            if count > 0:
-                if word in word_count:
-                    word_count[word] += count
-                else:
-                    word_count[word] = count
-    if data['data']['after'] is not None:
-        count_words(subreddit, word_list, data['data']['after'], word_count)
-    else:
-        sorted_words = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_words:
-            print(f"{word}: {count}")
+            counter[word] = 0
+
+    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
+    headers = {"User-Agent": "Python3"}
+    response = request("GET", url, headers=headers).json()
+    try:
+        top = response['data']['children']
+        _after = response['data']['after']
+        for item in top:
+            for word in counter:
+                counter[word] += item['data']['title'].lower(
+                    ).split(' ').count(word.lower())
+        if _after is not None:
+            count_words(subreddit, word_list, _after, counter, 1)
+        else:
+            str = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
+            for name, num in str:
+                if num != 0:
+                    print('{}: {}'.format(name, num))
+    except Exception:
+        return None
