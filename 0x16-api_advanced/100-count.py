cf82cae810c2652240f10e0b6d7bbc83cb327882
KagontleBooysen <k.booysen@alustudent.com>
2023-05-09 19:46:33 -0700

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,47 +1,32 @@
 #!/usr/bin/python3
 """Module for task 3"""
 
+import requests
 
-def count_words(subreddit, word_list, word_count={}, after=None):
-    """Queries the Reddit API and returns the count of words in
-    word_list in the titles of all the hot posts
-    of the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code != 200:
-        return None
-
-    info = sub_info.json()
-
-    hot_l = [child.get("data").get("title")
-             for child in info
-             .get("data")
-             .get("children")]
-    if not hot_l:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
-
-    for title in hot_l:
-        split_words = title.split(' ')
-        for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
-
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
+def count_words(subreddit, word_list, after=None, word_count=None):
+    if word_count is None:
+        word_count = {}
+    if after is None:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return
+    data = response.json()
+    for post in data['data']['children']:
+        title = post['data']['title'].lower()
+        for word in word_list:
+            count = title.count(word)
+            if count > 0:
+                if word in word_count:
+                    word_count[word] += count
+                else:
+                    word_count[word] = count
+    if data['data']['after'] is not None:
+        count_words(subreddit, word_list, data['data']['after'], word_count)
+    else:
+        sorted_words = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_words:
+            print(f"{word}: {count}")
